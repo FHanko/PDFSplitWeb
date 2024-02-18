e97c91c7f71d3fdf59d54ce91adb8b6c5975691d
FHanko <FlorianHanko@hotmail.com>
2024-02-18 02:26:11 +0100

    Add help page

--- src/app/tool-help/tool-help.component.ts
+++ src/app/tool-help/tool-help.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './tool-help.component.scss'
 })
 export class ToolHelpComponent {
-
+  pdfs = [
+    { title:"Tutorial.pdf", pages:7 },
+    { title:"Taxes1.pdf", pages:9 },
+    { title:"Taxes2.pdf", pages:25 },
+    { title:"Presentation.pdf", pages:11 }
+  ]
 }
