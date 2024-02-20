36578dfdf4b4db5d295361bacd0a0d2259502c5d
FHanko <FlorianHanko@hotmail.com>
2024-02-20 01:12:40 +0100

    Add pdf preview

--- src/app/pdf-tool/pdf-tool.component.ts
+++ src/app/pdf-tool/pdf-tool.component.ts
@@ -3,34 +3,35 @@ import { FormsModule } from '@angular/forms';
 import { PDFDocument } from 'pdf-lib';
 import { Parser, ParseResult, SyntaxErr } from './pdf-tool-parser';
 import { PDFUtil } from './pdf-util';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'pdf-tool',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgxExtendedPdfViewerModule],
   templateUrl: './pdf-tool.component.html',
   styleUrl: './pdf-tool.component.scss'
 })
 export class PdfToolComponent {
   getPDFs() {
-    return PDFUtil.docs;
+    return PDFUtil.docs
   }
 
   removePDF(pdf: PDFDocument) {
-    PDFUtil.docs = PDFUtil.docs.filter(p => p != pdf);
+    PDFUtil.docs = PDFUtil.docs.filter(p => p != pdf)
   }
 
   getError() {
-    return PDFUtil.error;
+    return PDFUtil.error
   }
 
   async onFileSelect(event: Event) {
-    const target = event.target as HTMLInputElement;
-    const fileList = target.files;
-    if (fileList == null) return;
+    const target = event.target as HTMLInputElement
+    const fileList = target.files
+    if (fileList == null) return
 
     for (let i = 0; i < fileList.length; i++) {
-      let file = fileList.item(i);
+      let file = fileList.item(i)
       if (file == null) continue;
       let buffer = await file.arrayBuffer()
       let doc = await PDFDocument.load(buffer)
@@ -42,26 +43,34 @@ export class PdfToolComponent {
   expression: string = ""
   parseErr: SyntaxErr[] = []
 
-  async generate() {
-    PDFUtil.error = "";
-    let x = new Parser(this.expression)
-    let result = x.parse()
+  async parseToDoc(): Promise<PDFDocument | undefined> {
+    PDFUtil.error = ""
+    const x = new Parser(this.expression)
+    const result = x.parse()
     this.parseErr = result.errs
+    return await result.ast?.doc
+  }
 
-    const bytes = await (await result.ast?.doc)?.save()
-    if (bytes == null) return;
-    this.downloadBlob(new Blob([new Uint8Array(bytes)]))
+  async download() {
+    const bytes = await (await this.parseToDoc())?.save()
+    if (bytes != undefined) this.downloadBlob(new Blob([bytes]))
+  }
+
+  previewSrc?: string
+  async preview() {
+    const b64 = await (await this.parseToDoc())?.saveAsBase64()
+    this.previewSrc = b64
   }
 
   downloadBlob(blob: Blob, name = 'file.pdf') {
-    const blobUrl = URL.createObjectURL(blob);
+    const blobUrl = URL.createObjectURL(blob)
   
-    const link = document.createElement("a");
+    const link = document.createElement("a")
   
-    link.href = blobUrl;
-    link.download = name;
+    link.href = blobUrl
+    link.download = name
   
-    document.body.appendChild(link);
+    document.body.appendChild(link)
   
     link.dispatchEvent(
       new MouseEvent('click', { 
@@ -71,6 +80,6 @@ export class PdfToolComponent {
       })
     );
   
-    document.body.removeChild(link);
+    document.body.removeChild(link)
   }
 }
